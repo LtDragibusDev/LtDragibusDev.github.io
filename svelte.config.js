f62b7878c55cf11fded986ae75b55aa244be0e8e
Elyne <LtDragibus@proton.me>
2026-06-10 15:53:19 +0200

    test 1

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV !== 'production';
+
 export default {
 	kit: {
 		adapter: adapter({
@@ -9,7 +11,7 @@ export default {
 		}),
 
 		paths: {
-			base: '/LtDragibusDev.github.io'
+			base: dev ? '' : '/LtDragibusDev.github.io'
 		},
 
 		prerender: {
